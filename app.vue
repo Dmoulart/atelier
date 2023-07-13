0317f06c48f98c34346ac1166c6ccb5df4ee6753
Dorian <dorian@resalocal.com>
2023-07-14 00:24:56 +0200

    Setup project

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <LazyElButton type="warning">lazy button</LazyElButton>
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
